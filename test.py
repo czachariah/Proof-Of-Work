eb6741aaf8c7777ae8846a75baae2a7a323d7cbb
czachariah <zachariahchris@yahoo.com>
2020-11-21 14:31:42 -0500

    fixed up part 2, ready for submission

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
 import sys
 import hashlib
-import itertools
-import time
-import string
 
 """
-This class will be used in order to create a proof-of-work string for a given message (in a file).
+This class will be used in order to check the proof-of-work.
 
-Input: nbits file
+Input: headerFile originalFile
 
-nbits = number of leading zero bits requested in the hash
-file = contains the message to hash and put leading zeros in
+headerFile = contains the header to be checked
+originalFile = contains the original message
 
 Author: Chris Zachariah
 """
@@ -20,28 +17,141 @@ BUF_SIZE = 65536
 
 
 def main():
-    listOfAllCharacters = string.printable
-    listOfAllCharacters = listOfAllCharacters.strip(' \t\n\r\x0b\x0c')
-    listOfAllCharacters = listOfAllCharacters.replace('\'', '')
-    listOfAllCharacters = listOfAllCharacters.replace('\"', '')
+    # check number of arguments
+    if len(sys.argv) != 3:
+        print("Usage: ./pow-check headerFile originalFile")
+        exit()
 
-    print(listOfAllCharacters)
+    # read from the headerFile and collect all the info needed
+    initialHash = ''
+    proof = ''
+    finalHash = ''
+    leading = ''
+    numReceived = 0
+    try:
+        with open(sys.argv[1], "r") as f:
+            for line in f:
+                if "Initial-hash:" in line:
+                    if initialHash == '':
+                        initialHash = line
+                        numReceived = numReceived + 1
+                    if numReceived == 4:
+                        break
+                if "Proof-of-work:" in line:
+                    if proof == '':
+                        proof = line
+                        numReceived = numReceived + 1
+                    if numReceived == 4:
+                        break
+                if "Hash:" in line:
+                    if finalHash == '':
+                        finalHash = line
+                        numReceived = numReceived + 1
+                    if numReceived == 4:
+                        break
+                if "Leading-bits:" in line:
+                    if leading == '':
+                        leading = line
+                        numReceived = numReceived + 1
+                    if numReceived == 4:
+                        break
+        f.close()
+    except IOError:
+        print("Error reading from", sys.argv[1])
+        exit()
 
-    r = 1
-    while r <= (len(listOfAllCharacters)):
-        combinations_object = itertools.combinations_with_replacement(listOfAllCharacters, r)       # get combos
-        for combo in combinations_object:
-            permutations_object = itertools.permutations(combo)                                     # get permutations
-            for proof in permutations_object:
-                i = 0
-        r = r + 1
+    # clean up all the data collected
+    if initialHash == '':
+        cleanedInitialHash = ''
+    else:
+        initialHash = initialHash.split(":")
+        cleanedInitialHash = initialHash[1].strip()
 
-    #print( " HERE ")
-    r = 1
-    while r <= (len(listOfAllCharacters)):
-        for p in itertools.product(listOfAllCharacters, repeat=r):
-            i = 0
-        r = r + 1
+    if proof == '':
+        cleanedProof = ''
+    else:
+        proof = proof.split(":")
+        cleanedProof = proof[1].strip()
+
+    if finalHash == '':
+        cleanedFinalHash = ''
+    else:
+        finalHash = finalHash.split(":")
+        cleanedFinalHash = finalHash[1].strip()
+
+    if leading == '':
+        cleanedLeading = ''
+    else:
+        leading = leading.split(":")
+        cleanedLeading = leading[1].strip()
+
+    hasPassed = True
+
+    # read from the originalFile and hash the message
+    sha256 = hashlib.sha256()
+    try:
+        with open(sys.argv[2], "rb") as f:
+            while True:
+                bytesBuf = f.read(BUF_SIZE)
+                if not bytesBuf:
+                    break
+                sha256.update(bytesBuf)
+        f.close()
+    except IOError:
+        print("Error reading from", sys.argv[2])
+        exit()
+    messageHash = sha256.hexdigest()
+
+    # see if the header has an initial hash and then check if it matches the file's hash
+    if len(cleanedInitialHash) == 0:
+        hasPassed = False
+        print("error: Missing Initial-hash from header.")
+    else:
+        if messageHash != cleanedInitialHash:
+            hasPassed = False
+            print("error: Header's Initial-hash does not match the file's hash")
+
+    # make sure the header has a proof of work
+    if len(cleanedProof) == 0:
+        hasPassed = False
+        print("error: Missing Proof-of-work from header.")
+        exit()
+
+    # compute the hash with the proof-of-work
+    proof = cleanedProof
+    conCat = proof + messageHash
+    result = hashlib.sha256(conCat.encode()).hexdigest()
+
+    # see if the header has the finalHash and then check if it matches with the result
+    if len(cleanedFinalHash) == 0:
+        hasPassed = False
+        print("error: Missing Hash from header")
+    else:
+        if result != cleanedFinalHash:
+            hasPassed = False
+            print("error: Header's Hash does not match the computed hash using the proof-of-work")
+
+    # compute the number of leading 0s
+    end_length = len(result) * 4
+    hex_as_int = int(result, 16)
+    hex_as_binary = bin(hex_as_int)
+    padded_binary = hex_as_binary[2:].zfill(end_length)
+    computedLeading = str(padded_binary.find('1'))
+
+    # see if the header has the leading 0s and then check if it matches with the computed value of leading 0s
+    if len(cleanedLeading) == 0:
+        hasPassed = False
+        print("error: Missing Leading-bits in header")
+    else:
+        if computedLeading != cleanedLeading:
+            hasPassed = False
+            print("error: Header's Leading-bits (" + cleanedLeading + ") does not match computed leading bits (" + computedLeading + ")")
+
+    # pass (no errors), fail(one or more errors)
+    if hasPassed is True:
+        print("pass")
+    else:
+        print("fail")
 
 
 if __name__ == "__main__":
